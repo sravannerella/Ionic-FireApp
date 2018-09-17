b5c2de8f1763b9e268f3be72998dba96ac928d30
sravannerella <sravannerella007@hotmail.com>
2018-09-17 09:30:36 -0400

    Maps integrated

--- app/js/controllers/mainCtrl.js
+++ app/js/controllers/mainCtrl.js
@@ -4,16 +4,34 @@
     angular.module('fireApp')
         .controller('mainCtrl', mainCtrl);
 
-    mainCtrl.$inject = ['$scope', '$stateParams', '$firebaseArray'];
+    mainCtrl.$inject = ['$scope', '$stateParams', '$firebaseArray', '$ionicPlatform', '$window'];
     
-    function mainCtrl($scope, $stateParams, $firebaseArray){
+    function mainCtrl($scope, $stateParams, $firebaseArray, $ionicPlatform, $window){
         var vm = this;
         vm.connected = true;
-        
+
+        $ionicPlatform.ready( ()=> {
+            console.log("READY");
+            loadMaps();
+        });
+
+        function loadMaps() {
+            var latLang = new google.maps.LatLng(37.3000, -120.4833);
+
+            var options = {
+                center: latLang,
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            var map = new google.maps.Map(document.getElementById("map"), options);
+            vm.map = map;
+        }
+
         $scope.$on('$ionicView.beforeEnter', () => {
             const rootRef = firebase.database().ref().child('Users');
             vm.object = $firebaseArray(rootRef);
             console.log("BEfore Entered", vm.object);
         });
-    }
+    }    
 })();
